da062511f89ef0fb98ce667dcc94657bdcdd9d62
Gabriel Lyon <glyon1@uc.cl>
2020-09-04 14:13:52 -0400

    feat(user-mutations): move update and delete user mutation to user mutations folder

---
+++ app/graphql/mutations/user_mutations/delete_user_mutation.rb
@@ -0,0 +1,18 @@
+module Mutations::UserMutations
+  class DeleteUserMutation < Mutations::BaseMutation
+    null true
+
+    # argument :user_id, ID, required: true, loads: Types::UserType
+    argument :id, ID, required: true
+
+    field :state, Boolean, null: true
+
+    def resolve(id:)
+      user = User.find(id)
+      user.delete
+      { state: true }
+    rescue ActiveRecord::RecordInvalid => e
+      GraphQL::ExecutionError.new("Invalid input: #{e.record.errors.full_messages.join(', ')}")
+    end
+  end
+end
--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -2,8 +2,8 @@ module Types
   class MutationType < Types::BaseObject
     field :login, mutation: Mutations::LoginMutation
     field :create_user, mutation: Mutations::UserMutations::CreateUserMutation
-    field :update_user, mutation: Mutations::UpdateUserMutation
-    field :delete_user, mutation: Mutations::DeleteUserMutation
+    field :update_user, mutation: Mutations::UserMutations::UpdateUserMutation
+    field :delete_user, mutation: Mutations::UserMutations::DeleteUserMutation
     field :create_post, mutation: Mutations::PostMutations::CreatePostMutation
     field :update_post, mutation: Mutations::PostMutations::UpdatePostMutation
   end
