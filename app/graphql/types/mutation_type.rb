0ecb39ee269e161719ffd70bb7e161cf8788828f
Gabriel Lyon <glyon1@uc.cl>
2020-09-04 09:39:57 -0400

    feat(types): delete hello world mutation and query type

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,11 +1,5 @@
 module Types
   class MutationType < Types::BaseObject
-    # TODO: remove me
-    field :test_field, String, null: false,
-                               description: "An example field added by the generator"
-    def test_field
-      "Hello World"
-    end
     field :login, mutation: Mutations::LoginMutation
     field :create_user, mutation: Mutations::UserMutations::CreateUserMutation
     field :update_user, mutation: Mutations::UpdateUserMutation
